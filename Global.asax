21900cc60078b04c36d8413de8709d6ab97fdeb1
Nenad Kocev <nkocevv@gmail.com>
2018-05-10 00:00:03 +0200

    add stores, albums, artists, datatables for albums

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="primerKolokviumska2.MvcApplication" Language="C#" %>
---
+++ Models/AddToStore.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace primerKolokviumska2.Models
+{
+    public class AddToStore
+    {
+        public List<Album> Albums { get; set; }
+        public int selectedAlbum { get; set; }
+        public int storeId { get; set; }
+
+        public AddToStore()
+        {
+            Albums = new List<Album>();
+        }
+    }
+}
